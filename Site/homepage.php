eafa1a06237d48e12af330a3c9de1b035e424488
Laetitia <laetitia.husson@student.yncrea.fr>
2020-11-10 11:04:11 +0100

    page d'accueil

---
+++ Site/homepage.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+
+
+    <head>
+
+        <link rel="stylesheet" href="CSS/body.css" />
+        <link rel="stylesheet" href="CSS/homepage.css" />
+
+    </head>
+    <body>
+        <?php include('header.php') ?>
+
+        
+        <?php include('footer.php') ?>
+        
+        
+            
+    </body>
+
+</html>
