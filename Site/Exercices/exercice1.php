d76ab063fb5873dcf23e74ca54db90c95c364b3e
Laetitia <laetitia.husson@student.yncrea.fr>
2020-11-12 11:59:16 +0100

    webcam actif

---
+++ Site/Exercices/exercice1.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+
+
+    <head>
+        <link rel="stylesheet" href="CSS/exercices.css" />
+        <link rel="stylesheet" href="CSS/body.css" />
+        <link rel="stylesheet" href="CSS/homepage.css" />
+
+    </head>
+    <body>
+        <?php include('header.php') ?>
+
+        
+        <div id="video">
+            //Coach
+            <div id="coach">
+
+            </div>
+
+            //utilisateur
+            <div id="utilisateur">
+            </div>
+        </div>
+        <?php include('footer.php') ?>
+        
+        
+            
+    </body>
+
+</html>
---
+++ Site/ajax_streaming_webcam/exercice1.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+
+
+    <head>
+        <link rel="stylesheet" href="../CSS/exercices.css" />
+        <link rel="stylesheet" href="../CSS/body.css" />
+        <link rel="stylesheet" href="../CSS/homepage.css" />
+
+    </head>
+    <body>
+        <?php include('../header.php') ?>
+
+        
+        <div id="video">
+            <!--//Coach-->
+            <div id="coach">
+                <video src="../exo6_boucle.mp4" controls muted width="350px" height="350px">
+                    Le fichier peut etre lu
+                </video>
+            </div>
+
+            <!--//utilisateur-->
+            <div id="coach">
+            <?php include('index.html') ?>
+                
+                
+            </div>
+        </div>
+        <?php include('../footer.php') ?>
+        
+        
+            
+    </body>
+
+</html>
